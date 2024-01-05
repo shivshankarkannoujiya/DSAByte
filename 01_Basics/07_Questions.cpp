5062881a24a911be029e1fef56d3182ff277c7ac
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-01-05 14:13:42 +0530

    Questions

---
+++ 01_Basics/07_Questions.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // 1: print name 11 times
+    for (int i = 0; i < 11; i++)
+    {
+        // cout << "Abhishek" << endl;
+    }
+
+    // 2: Table of 19
+    int num = 19; 
+    for (int i = 1; i <= 10; i++)
+    {
+        // cout << num << " X " << i << " : " << num * i << endl;
+    }
+
+    // 3: Print Even Number 1-100
+    for (int i = 1; i <= 100; i++)
+    {
+        if (i % 2 == 0)
+        {
+            cout << i << endl;
+        }
+    }
+}
