601b100622a9bb271e154eed21308aefdfe94891
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-01-05 13:41:17 +0530

    Loop

---
+++ 01_Basics/05_Loops.cpp
@@ -0,0 +1,41 @@
+/*
+for( initialization; condition <can be Multiple>; updation){
+    logic
+}
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // Ex:-1
+    cout<<"Ex:-1"<<endl;
+    for (int i = 1; i <= 5; i++)
+    {
+        cout << "Jai Mahakal💕🙏🏻" << endl;
+    }
+
+    // Ex:-2
+    cout<<"Ex:-2"<<endl;
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << i << endl;
+    }
+
+    // Ex:-3
+    cout<<"Ex:-3"<<endl;
+    for (int i = 10; i >= 0; i--)
+    {
+        cout << i << endl;
+    }
+
+    // Ex:-4
+    cout<<"Ex:-4"<<endl;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << i << endl;
+    }
+}
