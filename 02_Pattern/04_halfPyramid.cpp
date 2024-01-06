3e51303772a933ac4d0a740fc015a86755758582
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-01-06 14:13:01 +0530

    halfpyramid

--- 02_Pattern/04_halfPyramid.cpp
+++ 02_Pattern/04_halfPyramid.cpp
@@ -3,8 +3,10 @@ using namespace std;
 
 int main()
 {
-    
-    int TotalRows = 5;
+
+    int TotalRows;
+    cout << "Enter Total-Rows : ";
+    cin >> TotalRows;
     for (int row = 0; row < TotalRows; row++)
     {
         for (int j = 0; j < row + 1; j++)
