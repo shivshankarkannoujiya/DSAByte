a700828e9bfbd033b939ed8cd1232a7eae3738f2
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-01-08 14:44:42 +0530

    shiftOperator

--- 03_BitwiseOperator/02_shiftOperator.cpp
+++ 03_BitwiseOperator/02_shiftOperator.cpp
@@ -15,4 +15,17 @@ int main()
 
     // right-shift operator
     cout << "Ans ( >> ) is : " << (a >> 1) << endl;
+
+    // signed case : compiler handle thhis case
+    int n = -100;
+    cout << (n >> 1) << endl; //-50
+
+    // in case of unsigned : giving +ve large number
+    unsigned int num = -100;
+    cout << (num >> 1) << endl; // 2147483598
+
+    // shift count : -ve
+    // WARNING : shift count can not be -ve
+    // PRINT : Garbage value
+    cout << (a >> -1) << endl;
 }
