a7e5d8555024bfd0441bba1a7b3ad5264998f9dc
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-01-09 13:50:27 +0530

    prePost-operator

---
+++ 03_BitwiseOperator/03_Pre-Post.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Ex: 1
+    int a = 5;
+    ++a;
+    cout << a << endl; // 6
+
+    // Ex:2
+    // int a = 5;
+    // cout<<(++a)<<endl; //6
+
+    // Ex:3
+    // int a = 5;
+    // a++;
+    // cout << a << endl;  //6
+
+    // Ex:4
+    // int a = 5;
+    // cout << (a++) << endl; //5
+}
