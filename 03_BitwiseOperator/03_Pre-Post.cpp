dc2d4f8226a00a78c7497fa16cfd3e14137614b3
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-01-09 14:49:49 +0530

    Pre-Post Operator

--- 03_BitwiseOperator/03_Pre-Post.cpp
+++ 03_BitwiseOperator/03_Pre-Post.cpp
@@ -3,16 +3,16 @@ using namespace std;
 
 int main()
 {
-    // Ex: 1
-    int a = 5;
-    ++a;
-    cout << a << endl; // 6
+    // Ex: 1 -> Pre-Increment
+    // int a = 5;
+    // ++a;
+    // cout << a << endl; // 6
 
     // Ex:2
     // int a = 5;
     // cout<<(++a)<<endl; //6
 
-    // Ex:3
+    // Ex:3 -> Post-Increment
     // int a = 5;
     // a++;
     // cout << a << endl;  //6
@@ -20,4 +20,13 @@ int main()
     // Ex:4
     // int a = 5;
     // cout << (a++) << endl; //5
+
+    // Pre-Decrement
+
+    int a = 10;
+    cout << "Ans : <Pre-Increment> : " << (--a) * 10 << endl;
+
+    // Post-Increment
+    int b = 10;
+    cout << "Ans : <Post-Increment> : " << (b--) * 10 << endl;
 }
