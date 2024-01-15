505b7e2caf04db460a0ec4270b0bd2b61685a5e3
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-01-15 13:17:33 +0530

    pre-post

---
+++ 03_BitwiseOperator/04pre-post.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int a = 10;
+    // cout << (++a) * 10 << endl;
+    // cout << (a++) * 10 << endl;
+    // cout << a << endl;
+
+    // Ex:-2
+    // cout << (++a) * (a++) << endl;
+    cout << (a++) * (++a) << endl;
+}
