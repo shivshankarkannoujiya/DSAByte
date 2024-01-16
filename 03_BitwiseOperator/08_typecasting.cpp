c4c6aa1a6aad561a23c37a21fb8bd01f2a2c1e7f
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-01-16 17:16:13 +0530

    typecasting

--- 03_BitwiseOperator/08_typecasting.cpp
+++ 03_BitwiseOperator/08_typecasting.cpp
@@ -1,8 +1,31 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
+int main()
+{
 
-int main(){
+    // implicit TypeCasting :
+    /*
+     * Automatic type conversion
+     * Smaller type data --> larger type Data
+     */
 
-    
+    // Ex:- int --> float
+    int num1 = 10;
+    float num2 = 5.5;
+    float result = num1 + num2;
+    cout << "Ans is : " << result << endl;
+
+    // E:-2 char --> int
+    /*
+     * 'A' === ASCII_value (65) + 1 = 66
+     */
+    char ch = 'A';
+    int a = ch + 1;
+    cout << "Ans is : " << a << endl;
+
+    // Ex:3 int --> char
+    int var = 97;
+    char chh = var;
+    cout << "Ans is : " << chh << endl;
 }
