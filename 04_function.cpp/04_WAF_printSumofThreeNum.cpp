079538eeda996985d9e9e70f60eb58406b005cc2
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-01-18 13:50:47 +0530

    printSumOFThreeNum

--- 04_function.cpp/04_WAF_printSumofThreeNum.cpp
+++ 04_function.cpp/04_WAF_printSumofThreeNum.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 using namespace std;
 
+// Return sum of 3 Num
+
+int returnSum(int a, int b, int c)
+{
+    return a + b + b;
+}
+
+// Print sum of 3 Num
 void printSum(int a, int b, int c)
 {
     int sum = a + b + c;
-    cout << "Sum is " << sum << endl;
+    cout << "Sum is : " << sum << endl;
 }
 
 int main()
 {
+    // Calling function
+    int Result = returnSum(1, 2, 3);
+    cout << "Ans is : " << Result << endl;
+
     // Calling function
     printSum(1, 2, 3);
 }
