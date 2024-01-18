cbe06187d291720641ba5497b5282553dbdf7916
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-01-18 21:43:36 +0530

    numerica_hollow_inverted_half_pyramid

---
+++ 05_Assignment-1/01_numericalHollowHalfPyramid.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+void printNumericalHollowHalfPyramid(int n)
+{
+
+    for (int row = 0; row < n; row++)
+    {
+        for (int col = 0; col < row + 1; col++)
+        {
+            if (col == 0 || col == row || row == n - 1)
+            {
+                cout << col + 1;
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter totalRow : ";
+    cin >> n;
+
+    // calling function
+    printNumericalHollowHalfPyramid(n);
+}
