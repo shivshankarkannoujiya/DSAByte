13e90016deda56c309e1eb78be081febf264e4b7
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-01-18 13:31:44 +0530

    fn-call-stack

--- 04_function.cpp/03_fn-call-stack.cpp
+++ 04_function.cpp/03_fn-call-stack.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 using namespace std;
 
+void printA()
+{
+
+    cout << "Inside Fn-A" << endl;
+    cout << "Going back to Fn-main" << endl;
+}
 int main()
 {
-    
+
+    cout << "Inside Fn-main" << endl;
+    printA();
+    cout << "Back to Fn-main" << endl;
+    return 0;
 }
