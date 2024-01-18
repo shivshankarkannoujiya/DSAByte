a3169c2750ea1785336065584a9d77e7141241a3
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-01-18 13:04:25 +0530

    fn-basics

---
+++ 04_function.cpp/01_basics.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void printline()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "SpiderMan" << endl;
+    }
+}
+
+int main()
+{
+    // function call
+    printline();
+
+    return 0;
+}
