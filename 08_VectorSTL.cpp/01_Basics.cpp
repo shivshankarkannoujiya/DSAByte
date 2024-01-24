e9824631c6a7273e7b9290420ac3b03be93a0b47
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-01-24 21:37:53 +0530

    vector

---
+++ 08_VectorSTL.cpp/01_Basics.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Dynamic array
+    // !Dynamic memory allocation [M-1]
+    // int size;
+    // cout << "Enter size : ";
+    // int arr[size];
+
+    // !Dynamic memory allocation[M-2]
+    // int *arr = new int[size];
+
+
+    
+}
