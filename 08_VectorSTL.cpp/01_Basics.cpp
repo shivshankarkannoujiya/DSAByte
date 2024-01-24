f5a6f9d328354ea8728e0a229666020d53aa809f
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-01-24 22:27:44 +0530

    static and Dynamic Array

--- 08_VectorSTL.cpp/01_Basics.cpp
+++ 08_VectorSTL.cpp/01_Basics.cpp
@@ -1,17 +1,37 @@
 #include <iostream>
 using namespace std;
 
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
 int main()
 {
+
+    // static memory Allocation
+    // int arr[5] = {1,2,3,4,5};
+
     // Dynamic array
     // !Dynamic memory allocation [M-1]
-    // int size;
-    // cout << "Enter size : ";
+    int size;
+    cout << "Enter size : ";
+    cin >> size;
     // int arr[size];
 
     // !Dynamic memory allocation[M-2]
-    // int *arr = new int[size];
+    int *arr = new int[size];
 
+    // Takiing input
+    for (int i = 0; i < size; i++)
+    {
+        int data;
+        cin >> data;
+        arr[i] = data;
+    }
 
-    
+    printArray(arr, size);
 }
