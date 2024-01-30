4a0991fea34f8d9175e09279ef1daa953088e6f7
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-01-30 14:41:25 +0530

    find pivot index

---
+++ 11_Assignment-Questions/02_findPivotIndex.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int pivotIndex(vector<int> &nums)
+{
+    int n = nums.size();
+
+    for (int i = 0; i < n; i++)
+    {
+        int leftSum = 0;
+        int rightSum = 0;
+        
+        // leftSum
+        for (int j = 0; j < i; j++)
+        {
+            leftSum += nums[j];
+        }
+
+        // rightSum
+        for (int j = i + 1; j < n; j++)
+        {
+            rightSum += nums[j];
+        }
+
+        if (leftSum == rightSum)
+        {
+            return i;
+        }
+    }
+
+    // not find pivotIndex
+    return -1;
+}
+
+int main()
+{
+    vector<int> v = {1, 7, 3, 6, 5, 6};
+
+    // calling function
+    int PivotIndex = pivotIndex(v);
+    cout << "PivotIndex is : " << PivotIndex << endl;
+}
