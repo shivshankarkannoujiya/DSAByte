e9b98330370c55c144b24fab940ce8e4b930d67c
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-02-02 13:20:33 +0530

    find pivot index

--- 11_Assignment-Questions/02_findPivotIndex.cpp
+++ 11_Assignment-Questions/02_findPivotIndex.cpp
@@ -34,11 +34,45 @@ int pivotIndex(vector<int> &nums)
     return -1;
 }
 
+// method-2
+int prefixSumApproach(vector<int> &nums)
+{
+    int n = nums.size();
+
+    // create 2 array
+    vector<int>lsum(n,0);
+    vector<int>rsum(n,0);
+
+    // find lsum
+    for(int i =1;i<n;i++){
+        lsum[i]=lsum[i-1]+nums[i-1];
+    }
+
+    // find rsum
+    for(int i = n-2;i>=0;i--){
+        rsum[i] = rsum[i+1]+nums[i+1];
+    }
+
+    // checking 
+    for(int i = 0; i<n;i++){
+        if(lsum[i]==rsum[i]){
+            return i;
+        }
+    }
+
+    // not find 
+    return -1;
+
+    
+}
+
 int main()
 {
     vector<int> v = {1, 7, 3, 6, 5, 6};
 
     // calling function
-    int PivotIndex = pivotIndex(v);
-    cout << "PivotIndex is : " << PivotIndex << endl;
+    // int PivotIndex = pivotIndex(v);
+    // cout << "PivotIndex is : " << PivotIndex << endl;
+    int pivot = prefixSumApproach(v);
+    cout << "PivotIndex is : " << pivot << endl;
 }
